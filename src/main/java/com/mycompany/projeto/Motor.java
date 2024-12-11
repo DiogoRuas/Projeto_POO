0fcc40eff150a8d32441ca1848f11b16020bc46a
jfeiteira <jose.eduardo.feiteira@gmail.com>
2024-12-11 15:26:37 +0000

    Update class Motor

--- src/main/java/com/mycompany/projeto/Motor.java
+++ src/main/java/com/mycompany/projeto/Motor.java
@@ -9,5 +9,52 @@ package com.mycompany.projeto;
  * @author maria
  */
 public class Motor {
+
+    private int potencia;
+    private double cilindrada;
+    private double fuelTankCapacity;
+    private String combustivel;
+    private int consumo;
     
+    
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public double getFuelTankCapacity() {
+        return fuelTankCapacity;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+    
+    
+    public int getConsumo(){
+        return consumo;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public void setFuelTankCapacity(double fuelTankCapacity) {
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+    
+    
+    
+
 }
