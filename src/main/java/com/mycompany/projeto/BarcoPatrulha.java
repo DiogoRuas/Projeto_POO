2663324858409b56405ac26e862563ed9db9b7fa
Jose Feiteira <jose.eduardo.feiteira@gmail.com>
2025-01-15 13:02:55 +0000

    Update: Barco de Patrulha

--- src/main/java/com/mycompany/projeto/BarcoPatrulha.java
+++ src/main/java/com/mycompany/projeto/BarcoPatrulha.java
@@ -6,76 +6,92 @@ package com.mycompany.projeto;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+
 /**
  *
  * @author maria
  */
 class BarcoPatrulha extends Embarcacao {
-    
+
     private Motor motor;
     private boolean holofote;
-    private Radar radar;
-    
+    private RadarSimples radar;
+
     public BarcoPatrulha(int id, String nome, String marca, String modelo, LocalDate dataFabricacao) {
         super(id, nome, marca, modelo, dataFabricacao);
     }
-    
+
     @Override
-    public void ativarMissao(Zona zona, ArrayList<Marinheiro> tripulacao){
-        
-        if (this.inMissao == true){
-            throw new IllegalArgumentException("Lancha Rápida: Ja esta em missao");
+    public void ativarMissao(Zona zona, ArrayList<Marinheiro> tripulacao) {
+
+        if (this.inMissao == true) {
+            throw new IllegalArgumentException("Barco de Patrulha: Ja esta em missao");
         }
-        
-        if (tripulacao.size() < 2 || tripulacao.size() > 4){
-            throw new IllegalArgumentException("Lacha Rapida: Quantidade invalida de motores para. Tem de ter 2 a 4");
+
+        if (tripulacao.size() < 2 || tripulacao.size() > 4) {
+            throw new IllegalArgumentException("Barco de Patrulha: Quantidade invalida de motores para. Tem de ter 2 a 4");
         }
-        
-        boolean hasSargento = false;
+
+        boolean hasOficial = false;
+        int contadorOficiais = 0;
+
         for (Marinheiro m : tripulacao) {
-            if (m.getPatente() == Patente.SARGENTO) {
-                hasSargento = true;
-                break;
+            if (m.getPatente() == Patente.OFICIAL) {
+                contadorOficiais++;
             }
         }
 
-        if (!hasSargento) {
-            throw new IllegalArgumentException("Lancha Rápida: Deve haver pelo menos um sargento na tripulação.");
+        if (contadorOficiais != 1) {
+            throw new IllegalArgumentException("Barco de Patrulha: Deve haver exatamente um oficial na tripulação.");
         }
-        
-        
-        for (Marinheiro m : this.tripulacao) {
+
+        for (Marinheiro m : tripulacao) {
             m.setInMissao(true);
         }
-        
+
         this.inMissao = true;
         this.zona = zona;
         this.holofote = true;
         this.tripulacao = tripulacao;
-        
-        System.out.println("Lancha rapida " + "'" + this.nome + "': Estou em missao de captura");
+        this.radar = new RadarSimples();
+
+        System.out.println("Barco de Patrulha " + "'" + this.nome + "': Estou em missão de captura");
     }
-    
+
     @Override
-    public void terminarMissao(){
-        
-        if (this.inMissao == false){
-            throw new IllegalArgumentException("Lancha Rápida: Nao esta em missao");
+    public void terminarMissao() {
+
+        if (!this.inMissao) {
+            throw new IllegalArgumentException("Barco de patrulha: Não está em missão.");
         }
-        
+
         this.inMissao = false;
         this.zona = Zona.INDEF;
         this.holofote = false;
-        
+
         for (Marinheiro m : this.tripulacao) {
             m.setInMissao(false);
         }
-        
-        System.out.println("Lancha rapida " + "'" + this.nome + "': Terminei a missao de captura");
+
+        System.out.println("Barco de Patrulha " + "'" + this.nome + "': Terminei a missão de captura");
     }
-    
+
+    public void ativarRadar(ArrayList<Embarcacao> todasEmbarcacoes) {
+        if (!this.inMissao) {
+            throw new IllegalStateException("Barco de Patrulha: O radar só pode ser ativado durante uma missão.");
+        }
+        if (this.radar == null) {
+            throw new IllegalStateException("Barco de Patrulha: Radar não está instalado.");
+        }
+
+        this.radar.ligar();
+        ArrayList<Embarcacao> detectadas = radar.detectarEmbarcacoes(todasEmbarcacoes, this.zona);
+        this.radar.exibirInformacoesDeteccoes();
+    }
+
     @Override
     public String toString() {
         return "BarcoPatrulha[" + super.toString() + "]";
     }
+
 }
